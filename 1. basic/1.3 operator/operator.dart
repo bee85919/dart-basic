0b9b99307304d6b320fd34e0f45587c3ad933732
bee <bee85919@gmail.com>
2023-08-29 23:12:43 +0900

    Create operator.dart

---
+++ 1. basic/1.3 operator/operator.dart
@@ -0,0 +1,18 @@
+void main() {
+  
+  double number = 2;
+  
+  print(number + 2);
+  print(number - 2);
+  print(number * 2);
+  print(number / 2);
+  print(number % 2);
+  
+  number++; 
+  number--;
+  
+  number += 2; // 4
+  number -= 2; // 2
+  number *= 2; // 4
+  number /= 2; // 1
+}

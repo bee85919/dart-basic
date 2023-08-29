6df4714802b922cdf07867e3e6a4fc333045dfe4
bee <bee85919@gmail.com>
2023-08-29 23:08:23 +0900

    Create list.dart

---
+++ 1. basic/1.2 Collection/list.dart
@@ -0,0 +1,12 @@
+void main() {
+	List<String> blackPinkList = ['리사', '지수', '제니', '로제'];
+	
+	print(blackPinkList);
+	print(blackPinkList[0]);
+	print(blackPinkList[3]);
+
+	print(blackPinkList.length);
+
+	blackPinkList[3] = '코드팩토리';
+	print(blackPinkList);
+}
